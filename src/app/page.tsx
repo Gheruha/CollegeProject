cbd5c8487037d80a763eee6cd4369cb56e607ce8
Gheruha <maxgheruha@gmail.com>
2025-04-30 17:14:56 +0300

    Made the menu for navigation between pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
+
 export default function Home() {
   return (
-    // grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]
     <div className="">
       <section className="relative h-screen w-full">
         <Image
@@ -10,6 +10,7 @@ export default function Home() {
           fill
           className="object-cover"
         />
+
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="absolute inset-0 flex flex-col items-center justify-center">
           <h1 className="text-white text-4xl lg:text-6xl font-bold">
@@ -21,11 +22,7 @@ export default function Home() {
       <main
         className="w-full h-full flex gap-8 row-start-2 items-center 
       justify-between pl-40 pr-40 border"
-      >
-        <div></div>
-      </main>
-
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+      ></main>
     </div>
   );
 }
