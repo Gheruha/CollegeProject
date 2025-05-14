8d8a0688a0897a8c483ef5e5ba66eedc5200e139
Gheruha <maxgheruha@gmail.com>
2025-05-14 19:15:48 +0300

    Added more videos

--- src/app/students-work/db/videos.ts
+++ src/app/students-work/db/videos.ts
@@ -1,32 +1,27 @@
 export const videos = [
   {
     id: "video1",
-    title: "Proiect 1",
     src: "/videos/video1.mov",
     poster: "/posters/poster1.png",
   },
   {
     id: "video2",
-    title: "Proiect 2",
     src: "/videos/video2.mp4",
     poster: "/posters/poster2.png",
   },
   {
     id: "video3",
-    title: "Proiect 3",
     src: "/videos/video3.mp4",
     poster: "/posters/poster3.png",
   },
   {
     id: "video4",
-    title: "Proiect 4",
     src: "/videos/video4.mp4",
     poster: "/posters/poster4.png",
   },
   {
-    id: "video5",
-    title: "Proiect 5",
-    src: "/videos/video5.mov",
+    id: "interviu1",
+    src: "/videos/interviu1.mov",
     poster: "/posters/poster5.png",
   },
 ];
