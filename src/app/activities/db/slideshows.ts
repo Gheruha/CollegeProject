d1a70ed0ae50bbb7043c351c54e85fc7172d8df1
Gheruha <maxgheruha@gmail.com>
2025-05-15 23:22:37 +0300

    Added extracurricular activities

--- src/app/activities/db/slideshows.ts
+++ src/app/activities/db/slideshows.ts
@@ -25,4 +25,146 @@ export const slideshows: SlideshowMeta[] = [
       "/photos/a10.png",
     ],
   },
+  {
+    id: "activity4",
+    title:
+      'Medalionul literar-artistic„Mihai Eminescu - pașii spre eternitatez"',
+    images: [
+      "/photos/a11.png",
+      "/photos/a12.png",
+      "/photos/a13.png",
+      "/photos/a14.png",
+      "/photos/a15.png",
+      "/photos/a16.png",
+    ],
+  },
+  {
+    id: "activity5",
+    title: "Ziua Europei- Valori europene și naționale",
+    images: [
+      "/photos/a17.png",
+      "/photos/a18.png",
+      "/photos/a19.png",
+      "/photos/a20.png",
+    ],
+  },
+  {
+    id: "activity6",
+    title: "Ziua națională a lecturii maraton de poezie",
+    images: [
+      "/photos/a21.png",
+      "/photos/a22.png",
+      "/photos/a23.png",
+      "/photos/a24.png",
+      "/photos/a25.png",
+    ],
+  },
+  {
+    id: "activity7",
+    title: "In memoriam Doina și Ion aldea teodorovici - Două inimi gemene",
+    images: [
+      "/photos/a26.png",
+      "/photos/a27.png",
+      "/photos/a28.png",
+      "/photos/a29.png",
+    ],
+  },
+  {
+    id: "activity9",
+    title: "Festivalul „Focul din vatră, in memoriam Dumitru Matcovschi”",
+    images: [
+      "/photos/a30.png",
+      "/photos/a31.png",
+      "/photos/a32.png",
+      "/photos/a33.png",
+    ],
+  },
+  {
+    id: "activity10",
+    title: "Omagiul  „poetului cu lira-n lacrimi - Grigore Vieru”",
+    images: [
+      "/photos/a34.png",
+      "/photos/a35.png",
+      "/photos/a36.png",
+      "/photos/a37.png",
+    ],
+  },
+  {
+    id: "activity11",
+    title:
+      "Participarea la Concursul Internațional de Lectură și Creație PRO Lectura”",
+    images: [
+      "/photos/a38.png",
+      "/photos/a39.png",
+      "/photos/a40.png",
+      "/photos/a41.png",
+      "/photos/a42.png",
+      "/photos/a43.png",
+    ],
+  },
+  {
+    id: "activity12",
+    title: "Palmaresul  reușitelor”",
+    images: [
+      "/photos/a44.png",
+      "/photos/a45.png",
+      "/photos/a46.png",
+      "/photos/a47.png",
+      "/photos/a48.png",
+      "/photos/a49.png",
+    ],
+  },
+  {
+    id: "activity13",
+    title: "Palmaresul  reușitelor”",
+    images: [
+      "/photos/a50.png",
+      "/photos/a51.png",
+      "/photos/a52.png",
+      "/photos/a53.png",
+    ],
+  },
+  {
+    id: "activity14",
+    title: "Vizionarea spectacolelor",
+    images: [
+      "/photos/a54.png",
+      "/photos/a55.png",
+      "/photos/a56.png",
+      "/photos/a57.png",
+      "/photos/a58.png",
+    ],
+  },
+  {
+    id: "activity15",
+    title: "Activitate de diriginte - Voluntariat",
+    images: [
+      "/photos/a59.png",
+      "/photos/a60.png",
+      "/photos/a61.png",
+      "/photos/a62.png",
+    ],
+  },
+  {
+    id: "activity16",
+    title: "Călătorii",
+    images: [
+      "/photos/a63.png",
+      "/photos/a64.png",
+      "/photos/a65.png",
+      "/photos/a66.png",
+      "/photos/a67.png",
+      "/photos/a68.png",
+    ],
+  },
+  {
+    id: "activity17",
+    title: "Promoția 2019-2023",
+    images: [
+      "/photos/a69.png",
+      "/photos/a70.png",
+      "/photos/a71.png",
+      "/photos/a72.png",
+    ],
+  },
 ];
